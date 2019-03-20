1aac2ab4dbf25926d76fb2e8aba566aaadfcdd76
NguyenThiThanhHoa <donghainam.dhn@gmail.com>
2019-03-20 16:19:13 +0700

    2 files modified

--- src/app/form.component.ts
+++ src/app/form.component.ts
@@ -7,11 +7,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FormComponent  {
   nameContent="";
+  phoneNumber="";
   getName(value: string){
     this.nameContent=value;
   }
+  getPhone(value:string){
+    this.phoneNumber=value;
+  }
   @Output () OutputHandle = new EventEmitter<any>();
   pushContent(){
-    this.OutputHandle.emit(this.nameContent)
+    this.OutputHandle.emit(this.nameContent);
   }
 }
